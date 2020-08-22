dbd407e0842a7ab454fa6267c28186a320b8a789
Ruyi Li <ruyili2002@gmail.com>
2020-08-22 19:08:23 -0400

    feat: block saving and custom i/o

--- src/web/js/blockly-loader.js
+++ src/web/js/blockly-loader.js
@@ -6,6 +6,9 @@ import 'blockly/msg/en';
 import 'blockly/blockly_compressed';
 import 'blockly/blocks_compressed';
 
+import { BlocklyStorage } from './blockly/blockly-storage.js';
+import { compile } from './blockly/blockly-eval';
+
 const $ = document.querySelector.bind(document);
 // const embed = $('blockly-embed').shadowRoot;
 // const $_ = embed.querySelector.bind(embed);
@@ -39,8 +42,35 @@ function resizeBlocklyContainer (evt) {
 	Blockly.svgResize(workspace);
 }
 
-
 window.addEventListener('resize', resizeBlocklyContainer, false);
 
 resizeBlocklyContainer();
 Blockly.svgResize(workspace);
+
+$('#compile').addEventListener('click', () => compile(workspace));
+
+
+console.log(workspace);
+function save (evt) {
+	// Ignore events involving the UI like opening/closing
+	// the toolbox.
+	if (evt.type === Blockly.Events.UI) return;
+
+	try {
+		// Save to LS if a change was made
+		BlocklyStorage.backupBlocks_(workspace);
+		console.info('Sucessfully saved blocks to localStorage.');
+	} catch (err) {
+		console.error('Couldn\'t back up blocks to localStorage', err);
+	}
+}
+
+workspace.addChangeListener(save);
+
+// Automatically save when the user leaves the page
+// Disabled because it might cause some issues and overwrite
+// code if two instances of the page are open.
+// BlocklyStorage.backupOnUnload(workspace);
+
+// Restore blocks from a previous session from localStorage
+setTimeout(BlocklyStorage.restoreBlocks, 0);
