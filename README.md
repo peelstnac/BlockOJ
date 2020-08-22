acce33c28aebc8af3c88296a487ba9527cef7d2f
peelstnac <chfrn2002@gmail.com>
2020-08-21 22:40:50 -0400

    Added judge.c and sample test outputs to match

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 `npm run serve` - Start the frontend development server and opens the home page in the default browser. Runs on port 1234 by default. Supports HMR.
 
 `npm run dev` - Compiles source files and starts the web server. Runs on port 8000 by default.
+
+## How to run judge.c
+Compile if haven't already (if on linux, may have to use cc)
+```
+gcc judge.c -o judge.out
+```
+Then execute following on node.js (on linux, keep ./; on windows remove)
+```
+./judge.o [PATH TO USER OUTPUT] [PATH TO FOLDER WITH OFFICIAL OUTPUT] [TOTAL # OF TEST CASES]
+```
+ie.
+```
+./judge.o input.txt Tests/P1/ 2
+```
---
+++ judge.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <setjmp.h>
+
+#define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
+#define CATCH } else {
+#define ETRY } }while(0)
+#define THROW longjmp(ex_buf__, 1)
+
+typedef enum { false, true } bool;
+
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    // argv[1] is the path to the input file
+    // argv[2] is the path to the folder with solution cases (end with /)
+    if(argc != 4) {
+        fprintf(stderr, "argc != 4 but is %d\n", argc);
+        return 1;
+    }
+    // Total number of test cases
+    int total;
+    TRY {
+        sscanf(argv[3], "%d", &total);
+    }
+    CATCH {
+        fprintf(stderr, "argv[3] is not an integer but is %s", argv[2]);
+    } ETRY;
+    FILE* in = fopen(argv[1], "r");
+    bool skip = false;
+    for(int p=1; p<=total; p++) {
+        char end[50];
+        sprintf(end, "%d.txt", p);
+        char path[500];
+        strcpy(path, argv[2]);
+        strcat(path, end);
+        FILE* sol = fopen(path, "r");
+        char c1, c2;
+        for(;;) {
+            if(!skip) c1 = fgetc(in);
+            else skip = false;
+            c2 = fgetc(sol);
+            // Complete doubl EOF condition
+            if (c1 == EOF && c2 == EOF && p != total) {
+                printf("WA\n");
+                return 0;
+            }
+            if (c1 == EOF && c2 == EOF && p == total) {
+                printf("AC %d\nAC 0\n", p);
+                return 0;
+            }
+            // Check c1 EOF
+            if (c1 == EOF && c2 != EOF) {
+                printf("WA\n");
+                return 0;
+            }
+            // Check c2 EOF from different files
+            if (c1 != EOF && c2 == EOF && p == total) {
+                printf("WA\n");
+                return 0;
+            }
+            if (c1 != EOF && c2 == EOF && p != total) {
+                printf("AC %d\n", p);
+                skip = true;
+                break;
+            }
+            // Check no EOF
+            if (c1 != c2) {
+                printf("WA\n");
+                return 0; 
+            }
+        }
+    }
+}
