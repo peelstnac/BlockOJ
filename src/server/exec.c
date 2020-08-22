ae9d59a28a6400b20127ba2c5b40578d0d3f572e
peelstnac <chfrn2002@gmail.com>
2020-08-22 15:34:49 -0400

    Removed test files

--- src/server/exec.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main() {
-    //setvbuf(stdout, NULL, _IONBF, 0);
-    for (int i=0; i<10; i++) {
-        fprintf(stdout, "%d\n", i);
-        sleep(5);
-    }
-}
