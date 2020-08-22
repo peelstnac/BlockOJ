219c4fb6a33e93d921bd40dfcbddcee666a1e145
Ruyi Li <ruyili2002@gmail.com>
2020-08-21 22:15:28 -0400

    readme and server

---
+++ README.md
@@ -0,0 +1,4 @@
+## Scripts
+`npm run serve` - Start the frontend development server and opens the home page in the default browser. Runs on port 1234 by default. Supports HMR.
+
+`npm run dev` - Compiles source files and starts the web server. Runs on port 8000 by default.
---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', function (req: Request, res: Response) {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, function () {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
