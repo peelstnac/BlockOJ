122362d0cfd3083becbd64134517d62d8bead2b2
Ruyi Li <ruyili2002@gmail.com>
2020-08-22 20:32:17 -0400

    fix: some server/db code

---
+++ src/server/database.js
@@ -0,0 +1,34 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+pool.on('error', (err) => {
+	console.error(err);
+});
+
+async function query (queryString, params) {
+	const client = await pool.connect();
+	try {
+		const result = await client.query(queryString, params);
+		if (!result || !result.rows || !result.rowCount) return undefined;
+		return result.rows;
+	} finally {
+		client.release();
+	}
+}
+
+async function transact (queryString, params) {
+	const client = await pool.connect();
+	try {
+		await client.query('BEGIN');
+		const res = await client.query(queryString, params);
+		await client.query('COMMIT');
+	} catch (err) {
+		await client.query('ROLLBACK');
+		throw err;
+	} finally {
+		client.release();
+	}
+}
+
+module.exports = { query, transact };
--- src/server/server.js
+++ src/server/server.js
@@ -2,25 +2,26 @@
 'use strict';
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000 || 8000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const { v4: uuidv4 } = require('uuid');
 
-const { Pool, Client } = require('pg')
-// DB connection
-const pool = new Pool();
+const { query, transact } = require('./database');
+
 
 if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config();
 }
+
 // Dev gate
 if (app.get('env') === 'production') {
     app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
+    session.cookie.secure = true // serve secure cookies
 }
+
 // Mount session store
 app.use(session({
     store: new (require('connect-pg-simple')(session))(),
@@ -28,113 +29,106 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-    genid: (req) => {
-        return uuidv4();
-    }
+    genid: uuidv4,
 }));
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
     extended: true
 }));
 
+app.use(funcion (req, res) {
+
+});
+
 app.get('/', (req, res) => {
-    var sess = req.session;
-    if (sess.auth) res.sendFile(path.join(__dirname, 'views', 'root.html'));
+    if (req.session.auth) res.sendFile('index.html');
     else res.redirect('/login');
 });
 
 app.get('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect('/login');
+    res.redirect('/');
 });
 
 app.get('/login', (req, res) => {
-    var sess = req.session;
     // No need to login again
-    if (sess.auth) {
-        res.redirect('/');
-    }
-    else res.sendFile(path.join(__dirname, 'views', 'login.html'));
+    if (req.session.auth) res.redirect('/');
+    else res.sendFile('pages/login.html');
 });
 
-app.post('/login', (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
     req.body = {};
+
     // Search for the user
-    const text = 'SELECT password FROM users WHERE username=$1';
-    const values = [username];
-    pool.query(text, values, (err, r) => {
-        if (err) {
-            console.log("DEBUG: Something went wrong with query for username");
-            res.redirect('/login');
-        } else {
-            // Does the username exist
-            if (r.rows.length == 0) {
-                console.log("DEBUG: The username does not exist");
-                res.redirect('/login');
-            }
-            else {
-                // Compare password and hash
-                var hash = r.rows[0].password;
-                bcrypt.compare(password, hash, (err, verdict) => {
-                    if (err) {
-                        console.log(err);
-                        res.redirect('/login');
-                    } else {
-                        if (verdict == false) {
-                            res.redirect('/login');
-                        } else {
-                            // Successful login
-                            var sess = req.session;
-                            sess.auth = true;
-                            res.redirect('/');
-                        }
-                    }
-                });
-            }
-        }
-    });
+    const queryString = 'SELECT password FROM users WHERE username=$1';
+    const params = [ username ];
+
+    try {
+		const rows = await query(queryString, params);
+		if (!rows) {
+			// TODO Flash message
+			console.log("DEBUG: The username does not exist.");
+			res.redirect('/login');
+		} else {
+			const hash = rows[0].password;
+			bcrypt.compare(password, hash, (err, verdict) => {
+				if (err) {
+					console.log(err);
+					res.redirect('/login');
+				} else {
+					if (!verdict) {
+						res.redirect('/login');
+					} else {
+						// Successful login
+						req.session.auth = true;
+						res.redirect('/');
+					}
+				}
+			});
+		}
+	} catch (err) {
+		console.error("ERROR: Something went wrong with query for username.", err);
+		res.redirect('/login');
+	}
 });
 
 app.get('/register', (req, res) => {
     // No need to register again if already logged in
-    var sess = req.session;
-    if (sess.auth) {
-        res.redirect('/');
-    }
-    else res.sendFile(path.join(__dirname, 'views', 'register.html'));
+	// FIXME Not sure if we need the else? Idk if express stops the
+	// function as soon as we call redirect or whatever
+    if (req.session.auth) res.redirect('/');
+    else res.sendFile('pages/register.html');
 });
 
 const saltRounds = 10;
 
-app.post('/register', (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+app.post('/register',  (req, res) => {
+    const { username, password } = req.body;
     req.body = {};
-    var hash = bcrypt.hash(password, saltRounds, (err, hash) => {
+    const hash = bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
             console.log(err);
             res.redirect('/register');
         } else {
             // SQL Query
-            const text = 'INSERT INTO users(username, password) VALUES($1, $2)';
-            const values = [username, hash];
-            pool.query(text, values, (err, r) => {
-                if (err) {
-                    console.log("DEBUG: repeat username or bogus username/password (exceeds 50 chars)");
-                    res.redirect('/register');
-                } else {
-                    // Register successful
-                    console.log("DEBUG: register successful");
-                    res.redirect('/login');
-                }
-            });
+            const queryString = 'INSERT INTO users(username, password) VALUES($1, $2)';
+            const params = [username, hash];
+
+			try {
+				await transact(queryString, params);
+				// Register successful
+				console.log("DEBUG: register successful");
+				res.redirect('/login');
+			} catch (err) {
+				console.log("DEBUG: repeat username or bogus username/password (exceeds 50 chars)");
+				res.redirect('/register');
+			}
         }
     });
 });
 
 app.listen(PORT, function () {
-    console.log(`Listening on http://localhost:${PORT}`);
+	console.log(`Listening on http://localhost:${PORT}`);
 });
