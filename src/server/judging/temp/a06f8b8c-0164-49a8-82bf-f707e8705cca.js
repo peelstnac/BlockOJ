78b9dae802f9cab9a33d6461ab5d70781d8a11de
peelstnac <chfrn2002@gmail.com>
2020-08-23 01:52:30 -0400

    Updated README.md

---
+++ src/server/judging/temp/a06f8b8c-0164-49a8-82bf-f707e8705cca.js
@@ -0,0 +1,24 @@
+
+	  	// INSERT INTO BEGINNING OF EVERY SUBMISSION
+	  	const fs = require('fs');
+	  	var COMPILE_ln = 0;
+	  	var COMPILE_stdin = fs.readFileSync(0, 'utf-8').toString().split(/\r?\n/);
+	  	const window = {
+			prompt: () => {
+				if (COMPILE_ln + 1> COMPILE_ln.length) {
+					return '';
+				}
+      		return COMPILE_stdin[COMPILE_ln++];
+      		},
+	  	}
+	
+
+		var __MD792 = window.prompt();
+	  	while(__MD792--) {
+
+  	console.log('abc');
+
+
+  	}
+
+  	
